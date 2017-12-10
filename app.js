9235394745fa3505e0c31537b6e7bf7e8adb293d
Данил <vasiljev98@icloud.com>
2017-12-10 12:42:06 +0300

    DB setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://trump:123123@ds133296.mlab.com:33296/users');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
